3ebead53dfa4debd46c5dc5257efed0a1ff3881a
ashu2012 <atp.iitk@gmail.com>
2017-07-24 07:22:25 +0000

    review 3

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -257,7 +257,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent,learning= True , epsilon=1.0, alpha=0.99 )
+    agent = env.create_agent(LearningAgent,learning= False , epsilon=1.0, alpha=0.99 )
     
     ##############
     # Follow the driving agent
@@ -272,14 +272,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env,size=None, update_delay=0.01 , display= True , log_metrics=True, optimized= True )
+    sim = Simulator(env,size=None, update_delay=0.01 , display= True , log_metrics=True, optimized= False )
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(tolerance=0.005, n_test=0)
+    sim.run(tolerance=0.005, n_test=10)
     #sim.run()
 
 if __name__ == '__main__':
